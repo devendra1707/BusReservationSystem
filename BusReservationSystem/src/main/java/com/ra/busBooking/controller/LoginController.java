8c25200a63bbe4b154b99c0576fb093dbe94bf99
devendra1707 <pdevendra033@gmail.com>
2023-02-15 14:44:10 +0530

    Initial commit

---
+++ BusReservationSystem/src/main/java/com/ra/busBooking/controller/LoginController.java
@@ -0,0 +1,39 @@
+package com.ra.busBooking.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ra.busBooking.DTO.UserLoginDTO;
+
+import com.ra.busBooking.service.DefaultUserService;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+@Autowired
+	private DefaultUserService userService;
+
+    @ModelAttribute("user")
+    public UserLoginDTO userLoginDTO() {
+        return new UserLoginDTO();
+    }
+    
+	@GetMapping
+	public String login() {
+		return "login";
+	}
+	
+	@PostMapping
+	public void  loginUser(@ModelAttribute("user") 
+	UserLoginDTO userLoginDTO) {
+	 userService.loadUserByUsername(userLoginDTO.getUsername());
+	}
+}
